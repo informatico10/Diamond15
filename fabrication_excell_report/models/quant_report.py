5cf36d87a9685bfd574780fec0b53bb2499fe99e
DiamondOdoo15 <104848089+DiamondOdoo15@users.noreply.github.com>
2022-08-09 15:45:12 -0500

    Add files via upload

--- fabrication_excell_report/models/quant_report.py
+++ fabrication_excell_report/models/quant_report.py
@@ -79,7 +79,8 @@ class wizard_get_quants(models.Model):
 		cell_right_soles.set_font_name('Calibri')
 		cell_right_soles.set_font_size(11)
 		cell_right_soles.set_num_format('##0.00')
-		cell_right_soles.set_num_format('_-S/ * #,##0.00_-;S/ * -#,##0.00_-;_-S/ * "-"??_-;_-@_-')
+		cell_right_soles.set_num_format('S/ #,##0;S/ -#,##0')
+		#cell_right_soles.set_num_format('_-S/ * #,##0.00_-;S/ * -#,##0.00_-;_-S/ * "-"??_-;_-@_-')
 		#cell_right_soles.set_num_format('_-S * #,##0_-;S * -#,##0_-;_-S * "-"_-;_-@_-')
 		#cell_right_soles.set_num_format('_-S/ * #,##0_-;S/ * -#,##0_-;_-S/ * "-"_-;_-@_-')
 		#cell_right_soles.set_num_format('S/ #,##0;S/ -#,##0')
@@ -91,8 +92,9 @@ class wizard_get_quants(models.Model):
 		cell_right_dolares.set_border(1)
 		cell_right_dolares.set_font_name('Calibri')
 		cell_right_dolares.set_font_size(11)
-		cell_right_dolares.set_num_format('##0.00')
-		cell_right_dolares.set_num_format('_-$ * #,##0_-;$ * -#,##0_-;_-$ * "-"_-;_-@_-')
+		cell_right_soles.set_num_format('$ #,##0;$ -#,##0')
+		#cell_right_dolares.set_num_format('##0.00')
+		#cell_right_dolares.set_num_format('_-$ * #,##0_-;$ * -#,##0_-;_-$ * "-"_-;_-@_-')
 		#cell_right_dolares.set_num_format('$/ #,##0;$/ -#,##0')
 
 
@@ -108,7 +110,8 @@ class wizard_get_quants(models.Model):
 		cell_numero.set_border(1)
 		cell_numero.set_font_name('Calibri')
 		cell_numero.set_font_size(11)
-		cell_numero.set_num_format('_-S/ * #,##0.00_-;S/ * -#,##0.00_-;_-S/ * "-"??_-;_-@_-')
+		cell_right_soles.set_num_format('S/ #,##0;S/ -#,##0')
+		#cell_numero.set_num_format('_-S/ * #,##0.00_-;S/ * -#,##0.00_-;_-S/ * "-"??_-;_-@_-')
 		#cell_numero.set_num_format('_-S/ * #,##0_-;S/ * -#,##0_-')
 		#cell_numero.set_num_format('"S/" #,##0.00')
 
@@ -124,7 +127,8 @@ class wizard_get_quants(models.Model):
 		cell_numero_dolar.set_border(1)
 		cell_numero_dolar.set_font_name('Calibri')
 		cell_numero_dolar.set_font_size(11)
-		cell_numero_dolar.set_num_format('_-$ * #,##0_-;$ * -#,##0_-;_-$ * "-"_-;_-@_-')
+		cell_right_soles.set_num_format('$ #,##0;$ -#,##0')
+		#cell_numero_dolar.set_num_format('_-$ * #,##0_-;$ * -#,##0_-;_-$ * "-"_-;_-@_-')
 		#cell_numero_dolar.set_num_format('$#,##0.00')
 
 		import datetime
