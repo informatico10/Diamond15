7ee3c558d09295c0bccdba6d8cf4c4c0a24b5e0e
daveRQ <42696677+daveRQ@users.noreply.github.com>
2022-08-23 09:41:37 -0500

    firma reporte

---
+++ users_firma_rq_it/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Users Firma RQ IT',
+    'version': '1.0',
+    'description': 'Users Firma RQ IT',
+    'author': 'ITGRUPO',
+    'license': 'LGPL-3',
+    'category': 'sale',
+    'auto_install': False,
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'views/users.xml',
+    ],
+    'installable': True
+}
