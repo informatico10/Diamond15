8bd338b24d69ddb4919e0b8cc3070dd008ec7f89
daveRQ <42696677+daveRQ@users.noreply.github.com>
2022-08-24 15:59:43 -0500

    update

--- users_firma_rq_it/__manifest__.py
+++ users_firma_rq_it/__manifest__.py
@@ -1,18 +1,15 @@
 {
-    'name': 'Purchase Report PDF Diamond RQ IT',
+    'name': 'Users Firma RQ IT',
     'version': '1.0',
-    'description': 'Purchase Report PDF Diamond RQ IT',
+    'description': 'Users Firma RQ IT',
     'author': 'ITGRUPO',
     'license': 'LGPL-3',
     'category': 'sale',
     'auto_install': False,
     'depends': [
         'purchase',
-        'account_fields_it'
     ],
     'data': [
-        'security/security.xml',
-
         'views/users.xml',
     ],
     'installable': True
