ee797acc33710d95266a34d96b4f312fadb7bed6
Glenda Julia <glenda.merma@tecsup.edu.pe>
2022-09-06 16:32:17 -0500

    a

--- om_account_asset_diamond/models/account_asset.py
+++ om_account_asset_diamond/models/account_asset.py
@@ -8,3 +8,22 @@ class AccountAssetAsset(models.Model):
 
 	area = fields.Char(string='Area')
 	encargado = fields.Char(string='Encargado')
+
+	@api.model
+	def create(self, vals):
+		id_seq = self.env['ir.sequence'].search([('name', '=', 'Activos Diamond IT'),('company_id','=',self.env.company.id)],limit=1)
+
+		if not id_seq:
+			id_seq = self.env['ir.sequence'].create({'name': 'Activos Diamond IT', 
+			'company_id': self.env.company.id, 
+			'implementation': 'no_gap',
+			'active': True, 
+			'prefix': '', 
+			'padding': 4, 
+			'number_increment': 1, 
+			'number_next_actual': 1})
+
+		code = id_seq._next()
+		vals['code'] = '1' + vals['code'].strftime('%y%m') + code
+		t = super(AccountAssetAsset, self).create(vals)
+		return t
