c62943623f212bb39358cb19c62762f6cdc2ad80
daveRQ <42696677+daveRQ@users.noreply.github.com>
2022-09-06 19:23:54 -0500

    update

--- purchase_report_pdf_diamond_rq_it/models/payment.py
+++ purchase_report_pdf_diamond_rq_it/models/payment.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 class AccountPaymentTermReportPdf(models.Model):
     _inherit = 'account.payment.term'
 
-    name_english = fields.Char(string='Nombre Ingles', required=False)
+    name_english = fields.Char(string='Nombre Ingles / Español', required=False)
